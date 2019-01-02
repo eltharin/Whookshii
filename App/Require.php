36d0344b62a0fb33726b4654e0070dea128819cf
eltharin <eltharin18@hotmail.fr>
2019-01-02 21:47:48 +0100

    suppr old file

--- App/Require.php
+++ App/Require.php
@@ -4,8 +4,6 @@ require_once APP . 'Define.php';
 
 require CORE . 'Core.php';
 require APP . 'Functions.php';
-require CORE . 'App/Config/Routes.php';
-require CORE . 'App/Config/HTMLPage.php';
 require CORE . 'Config.php';
 require CORE . 'App' . DS . 'Loader.php';
 require_once APP . 'log.php';
