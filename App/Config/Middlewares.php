f16527241903004e53cd5862205e3a5171dbda90
eltharin <eltharin18@hotmail.fr>
2019-10-28 16:17:03 +0100

    manage default values

--- App/Config/Middlewares.php
+++ App/Config/Middlewares.php
@@ -6,11 +6,11 @@ class Middlewares extends ConfigElementAbstract
 	protected const AUTOFILECONFIG = 'auto.middlewares';
 
 	protected $config = [
-									\Core\App\Middleware\Subfolder::class,
+									//\Core\App\Middleware\Subfolder::class,
 									\Core\App\Middleware\FileLoader::class,
-									//\Core\App\Middleware\Config::class,
+									\Core\App\Middleware\Config::class,
 									\Core\App\Middleware\Router::class,
-
+									\Core\App\Middleware\Templater::class,
 									\Core\App\Middleware\Launcher::class
 							];
 }
