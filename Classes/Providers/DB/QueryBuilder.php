6b3b5f9c4c4eff12862ca0d946c20e7a0865e549
Eltharin <eltharin18@hotmail.fr>
2020-12-21 17:35:22 +0100

    add key pair

--- Classes/Providers/DB/QueryBuilder.php
+++ Classes/Providers/DB/QueryBuilder.php
@@ -429,7 +429,7 @@ class QueryBuilder implements \Iterator
 		{
 			return $all;
 		}
-		if($this->fetchMode !== null && ($this->fetchMode[0] != \PDO::FETCH_OBJ && $this->fetchMode[0] & (\PDO::FETCH_OBJ | \PDO::FETCH_GROUP) == 0))
+		if($this->fetchMode !== null && ($this->fetchMode[0] == \PDO::FETCH_KEY_PAIR || $this->fetchMode[0] != \PDO::FETCH_OBJ && $this->fetchMode[0] & (\PDO::FETCH_OBJ | \PDO::FETCH_GROUP) == 0))
 		{
 			return $all;
 		}
